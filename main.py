6fb00272e9189c1e77e81e893adb26e3276194b7
ohtoe02 <ohtoe02@gmail.com>
2025-06-18 15:29:17 +0500

    1

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+"""Модуль main.py: пример использования Pylint."""
+
 import ast
 
 
 def multiply_and_add(number):
+    """Функция умножает число на 10 и прибавляет само число."""
     return number * 10 + number
 
 
 def main():
+    """Основная функция программы."""
     user_input = input("Введите число: ")
     number = ast.literal_eval(user_input)  # Безопасное преобразование
     result = multiply_and_add(number)
